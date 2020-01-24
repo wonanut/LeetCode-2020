3c73f7ff20ae22e138690cb12dffaa2015770f07
Howard_YT <1343121616@qq.com>
2020-01-24 11:06:14 +0800

    update 236

--- src/2020-01/week4/236-lowest-common-ancestor-of-a-binary-tree.py
+++ src/2020-01/week4/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -17,18 +17,34 @@
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         def helper(root):
+            # If reached the end of a branch, return False.
             if not root:
                 return False
 
+            # Left and Right Recursion
             left = helper(root.left)
             right = helper(root.right)
+
+            # If the current node is one of p or q
             mid = p == root or q == root
 
+            # If any two of the three flags left, right or mid become True.
             if mid + left + right >= 2:
                 ans[0] = root
             
+            # Return True if either of the three bool values is True.
             return mid or left or right
         
         ans = [None]
         helper(root)
         return ans[0]
+
+    # 另一种方法太牛皮
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        # Return root if left or right is False, otherwise left or right
+        return root if (left and right) else (left or right)
